69d78b2caa0883a48af7884b9c7afb98e458d027
angeldahiya <dahiya.angel27@gmail.com>
2020-11-25 14:39:23 +0530

    Create helloworld_start.py
    changed

---
+++ python/helloworld_start.py
@@ -0,0 +1,13 @@
+#
+# Example file for HelloWorld
+#
+print ("hello world!")
+
+def main ():
+    print ("hello mithu!")
+    #name = input("what is your name? ")
+    #print("Nice to meet you,", name)
+
+
+    if __name__ == "__main__":
+main()
