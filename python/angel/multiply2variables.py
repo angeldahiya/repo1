c1bc7248321c9c63be27457b989e4a368e8401eb
angeldahiya <dahiya.angel27@gmail.com>
2021-01-27 14:06:08 +0530

    multiplied 2 variables

---
+++ python/angel/multiply2variables.py
@@ -0,0 +1,7 @@
+# define 2 variables then assign to numeric values to those variables 
+# then multiply those 2 variables and store the result in third variable 
+# print 3rd variable
+number1 = 1888999441354757633
+number2 = 82654921886540987543
+number3 = number1 *number2
+print(number3)
