35049030520306ee30001862f368ea1c2cff7eee
angeldahiya <dahiya.angel27@gmail.com>
2021-01-27 14:15:02 +0530

    divided 2 variables

---
+++ python/angel/divide2variables.py
@@ -0,0 +1,7 @@
+# define 2 variables then assign to numeric values to those variables 
+# then divide those 2 variables and store the result in third variable 
+# print 3rd variable
+number1 = 188899
+number2 = 82654
+number3 = number1 /number2
+print(number3)
